7443f4d268fb1f69db31228cfeb51ba693d47abc
komfysach <sach@getkomfy.net>
2021-06-23 20:36:12 +0200

    added User and Roles CRUD

--- database/connect.go
+++ database/connect.go
@@ -18,6 +18,6 @@ func Connect() {
 
 	DB = database
 
-	database.AutoMigrate(&models.User{})
+	database.AutoMigrate(&models.User{}, &models.Role{})
 
 }
--- routes/routes.go
+++ routes/routes.go
@@ -21,4 +21,10 @@ func Setup(app *fiber.App) {
 	app.Get("/api/users/:id", controllers.GetUser)
 	app.Put("/api/users/:id", controllers.UpdateUser)
 	app.Delete("/api/users/:id", controllers.DeleteUser)
+
+	app.Get("/api/roles", controllers.AllRoles)
+	app.Post("/api/roles", controllers.CreateRole)
+	app.Get("/api/roles/:id", controllers.GetRole)
+	app.Put("/api/roles/:id", controllers.UpdateRole)
+	app.Delete("/api/roles/:id", controllers.DeleteRole)
 }
