158fcf4c12e61e3bd3e0d995eeeb5d5a6d275fc1
komfysach <sach@getkomfy.net>
2021-06-24 11:12:29 +0200

    added User Profile updates

--- routes/routes.go
+++ routes/routes.go
@@ -13,6 +13,9 @@ func Setup(app *fiber.App) {
 
 	app.Use(middlewares.IsAuthenticated)
 
+	app.Put("/api/users/info", controllers.UpdateInfo)
+	app.Put("/api/users/password", controllers.UpdatePassword)
+
 	app.Get("/api/user", controllers.User)
 	app.Post("/api/logout", controllers.Logout)
 
